9b60630760dc34621076b161c6f1c583ce8e8801
Henry Onyango <twiga@dru.twiga.com>
2020-07-14 12:59:30 +0300

    configure babel and webpack

--- js/App.jsx
+++ js/App.jsx
@@ -1,44 +1,22 @@
+import React from "react";
+import { render } from "react-dom";
+
 const SecondComponent = ({ title }) => {
-                                                                                                                        return React.createElement(
-                                                                                                                                                                                                                                                'div',
-                                                                                                                                                                                                                                                {},
-                                                                                                                                                                                                                                                React.createElement(
-                                                                                                                                                                                                                                                                                                                                                                        'h2',
-                                                                                                                                                                                                                                                                                                                                                                        {},
-                                                                                                                                                                                                                                                                                                                                                                        title
-                                                                                                                                                                                                                                                )
-                                                                                                                        );
+  return React.createElement("div", {}, React.createElement("h2", {}, title));
 };
 
 const FirstComponent = () => {
-                                                                                                                        return React.createElement(
-                                                                                                                                                                                                                                                'div',
-                                                                                                                                                                                                                                                {},
-                                                                                                                                                                                                                                                React.createElement(
-                                                                                                                                                                                                                                                                                                                                                                        'h1',
-                                                                                                                                                                                                                                                                                                                                                                        {},
-                                                                                                                                                                                                                                                                                                                                                                        'Heading'
-                                                                                                                                                                                                                                                ),
-                                                                                                                                                                                                                                                React.createElement(
-                                                                                                                                                                                                                                                                                                                                                                        SecondComponent,
-                                                                                                                                                                                                                                                                                                                                                                        {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                title: 'Rick and Morty'
-                                                                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                ),
-                                                                                                                                                                                                                                                React.createElement(
-                                                                                                                                                                                                                                                                                                                                                                        SecondComponent,
-                                                                                                                                                                                                                                                                                                                                                                        {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                title: 'Silicon Valley'
-                                                                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                )
-                                                                                                                        );
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Heading"),
+    React.createElement(SecondComponent, {
+      title: "Rick and Morty",
+    }),
+    React.createElement(SecondComponent, {
+      title: "Silicon Valley",
+    })
+  );
 };
 
-ReactDOM.render(
-                                                                                                                        React.createElement(
-                                                                                                                                                                                                                                                FirstComponent
-                                                                                                                        ),
-                                                                                                                        document.getElementById(
-                                                                                                                                                                                                                                                'app'
-                                                                                                                        )
-);
+render(React.createElement(FirstComponent), document.getElementById("app"));
