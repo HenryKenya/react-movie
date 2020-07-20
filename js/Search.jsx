1746ce1ae41cb409ca62cebe25ec0a5b028aa68d
Henry Onyango <twiga@dru.twiga.com>
2020-07-20 11:43:59 +0300

    update babel, implement search

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import preload from "../data.json";
 import ShowCard from "./ShowCard";
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map((show) => (
-        <ShowCard {...show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-);
+class Search extends Component {
+  state = {
+    searchTerm: "",
+  };
+  handleSearchTermChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            type="text"
+            placeholder="Search"
+            value={this.state.searchTerm}
+            onChange={this.handleSearchTermChange}
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              (show) =>
+                `${show.title} ${show.description}`
+                  .toLocaleLowerCase()
+                  .indexOf(this.state.searchTerm) >= 0
+            )
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID} />
+            ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
