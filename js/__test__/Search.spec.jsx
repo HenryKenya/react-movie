f9bcb6c4bd510da646e089bf075adc9a57c3f7a1
Henry Onyango <dru@Twigas-MBP-2.twiga.com>
2020-07-21 11:57:58 +0300

    update tests

--- js/__test__/Search.spec.jsx
+++ js/__test__/Search.spec.jsx
@@ -1,9 +1,33 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
+import preload from "../../data.json";
 import Search from "../Search";
+import ShowCard from "../ShowCard";
 
-test("Search renders correctly", () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+describe("Search", () => {
+  it("renders correctly", () => {
+    const component = shallow(<Search />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it("should render correct amount of shows given no search term", () => {
+    const component = shallow(<Search />);
+    expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+  });
+
+  it("should render correct amount of shows based on the search term given", () => {
+    const searchWord = "black";
+    const component = shallow(<Search />);
+    const showCount = preload.shows.filter(
+      (show) =>
+        `${show.title} ${show.description}`
+          .toLowerCase()
+          .indexOf(searchWord.toLowerCase()) >= 0
+    ).length;
+    component
+      .find("input")
+      .simulate("change", { target: { value: searchWord } });
+
+    expect(component.find(ShowCard).length).toEqual(showCount);
+  });
 });
