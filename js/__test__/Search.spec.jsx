e67bdb516af28aa365501e8b746af8d0674733a8
Henry Onyango <dru@Twigas-MBP-2.twiga.com>
2020-07-23 09:24:11 +0300

    update search tests

--- js/__test__/Search.spec.jsx
+++ js/__test__/Search.spec.jsx
@@ -1,3 +1,4 @@
+// @flow
 import React from "react";
 import { shallow } from "enzyme";
 import preload from "../../data.json";
@@ -6,18 +7,18 @@ import ShowCard from "../ShowCard";
 
 describe("Search", () => {
   it("renders correctly", () => {
-    const component = shallow(<Search />);
+    const component = shallow(<Search shows={preload.shows} />);
     expect(component).toMatchSnapshot();
   });
 
   it("should render correct amount of shows given no search term", () => {
-    const component = shallow(<Search />);
+    const component = shallow(<Search shows={preload.shows} />);
     expect(component.find(ShowCard).length).toEqual(preload.shows.length);
   });
 
   it("should render correct amount of shows based on the search term given", () => {
     const searchWord = "black";
-    const component = shallow(<Search />);
+    const component = shallow(<Search shows={preload.shows} />);
     const showCount = preload.shows.filter(
       (show) =>
         `${show.title} ${show.description}`
