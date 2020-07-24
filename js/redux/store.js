796c3d7ce81f1d528517634dbad24a0911a202dc
Henry Onyango <dru@Henry-MacBook-Pro.local>
2020-07-24 13:13:14 +0300

    check for state mutability

--- js/redux/store.js
+++ js/redux/store.js
@@ -1,14 +1,10 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import reducer from "./reducers";
 
-// $FlowFixMe
-// eslint-disable-next-line no-underscore-dangle
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const store = createStore(
   reducer,
-  composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+  applyMiddleware(reduxImmutableStateInvariant())
 );
 
 export default store;
