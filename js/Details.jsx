f66bbeea3daa37e239ffccd16d14e84ff35921f9
Henry Onyango <dru@Twigas-MBP-2.twiga.com>
2020-07-27 10:46:49 +0300

    implement thunk for fetching ratings

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,30 +1,25 @@
 // @flow
 import React, { Component } from "react";
-import axios from "axios";
+import { connect } from "react-redux";
+import { getAPIData } from "./redux/actionCreators";
 import Header from "./Header";
 import Spinner from "./Spinner";
 
 class Details extends Component {
-  state = {
-    apiData: { rating: "" },
-  };
-
   componentDidMount() {
-    axios
-      .get(`http://localhost:3000/${this.props.show.imdbID}`)
-      .then((response: { data: { rating: string } }) => {
-        this.setState({ apiData: response.data });
-      });
+    if (!this.props.rating) this.props.getAPIData();
   }
 
   props: {
     show: Show,
+    rating: string,
+    getAPIData: Function,
   };
 
   render() {
     let ratingComponent;
-    if (this.state.apiData.rating) {
-      ratingComponent = <h3>{this.state.apiData.rating}</h3>;
+    if (this.props.rating) {
+      ratingComponent = <h3>{this.props.rating}</h3>;
     } else {
       ratingComponent = <Spinner />;
     }
@@ -55,4 +50,17 @@ class Details extends Component {
   }
 }
 
-export default Details;
+const mapStateToProps = (state, ownProps) => {
+  const apiData = state.apiData[ownProps.show.imdbID]
+    ? state.apiData[ownProps.show.imdbID]
+    : { rating: "" };
+  return { rating: apiData.rating };
+};
+
+const mapDispatchToProps = (dispatch: Function, ownProps) => ({
+  getAPIData() {
+    dispatch(getAPIData(ownProps.show.imdbID));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Details);
--- js/redux/store.js
+++ js/redux/store.js
@@ -1,6 +1,7 @@
 // @flow
 import { createStore, compose, applyMiddleware } from "redux";
 import immutableStateInvariantMiddleware from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 import reducer from "./reducers";
 
 // eslint-disable-next-line no-underscore-dangle
@@ -8,7 +9,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducer,
-  composeEnhancers(applyMiddleware(immutableStateInvariantMiddleware()))
+  composeEnhancers(applyMiddleware(immutableStateInvariantMiddleware(), thunk))
 );
 
 export default store;
