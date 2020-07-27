c6ad036aac0d2a56b38689476cc6b82fb57aacfb
Henry Onyango <dru@Twigas-MBP-2.twiga.com>
2020-07-27 10:05:10 +0300

    add devTools to store

--- js/redux/store.js
+++ js/redux/store.js
@@ -1,11 +1,14 @@
 // @flow
-import { createStore, applyMiddleware } from "redux";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import { createStore, compose, applyMiddleware } from "redux";
+import immutableStateInvariantMiddleware from "redux-immutable-state-invariant";
 import reducer from "./reducers";
+
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducer,
-  applyMiddleware(reduxImmutableStateInvariant())
+  composeEnhancers(applyMiddleware(immutableStateInvariantMiddleware()))
 );
 
 export default store;
